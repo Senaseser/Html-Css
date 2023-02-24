50ac09ea1ef49578965e8dd152118f1212bb64f6
Senaseser <112764298+Senaseser@users.noreply.github.com>
2023-02-24 17:29:57 +0300

    Html-Css sample web page design

---
+++ Sena/script.js
@@ -0,0 +1,10 @@
+// window.addEventListener("load", (event) => {
+//     console.log("page is fully loaded");
+//   });
+//   function showDiv(id) {
+//     var div = document.getElementById(id);
+//     div.style.display = "block";
+// }
+if(2 == 2){
+  console.log("Eşit");
+}
